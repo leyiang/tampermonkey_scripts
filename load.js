58020f94232b8a513cf5fc3e0266151fec8e6848
Yiang <yiangforwork@gmail.com>
2024-11-25 18:35:04 +0800

    update: load script
    1. remove test log code
    2. replace eval with Function (able to pass args)

--- load.js
+++ load.js
@@ -1,33 +1,33 @@
 function exec_script(path) {
-	var nodeJSEndPoint = path + '?time=' + Date.now();
-			console.log('[test] start');
+    var nodeJSEndPoint = path + '?time=' + Date.now();
+    console.log('[test] start');
 
-	// Function to evaluate the fetched script
-	function evaluateScript(scriptContent) {
-        console.log( scriptContent );
-		eval(scriptContent);
-	}
+    // Function to evaluate the fetched script
+    function evaluateScript(scriptContent) {
+        const func = new Function( scriptContent );
+        func.call( null, path );
+    }
 
-	// Function to handle the GM_xmlhttpRequest response
-	function handleResponse(response) {
-        			console.log('[test] on load');
-		if (response.status === 200 && response.responseText) {
-			evaluateScript(response.responseText);
-		} else {
-			console.log('[test] Error loading Node.JS Script:', response.statusText);
-		}
-	}
+    // Function to handle the GM_xmlhttpRequest response
+    function handleResponse(response) {
+        console.log('[test] on load');
+        if (response.status === 200 && response.responseText) {
+            evaluateScript(response.responseText);
+        } else {
+            console.log('[test] Error loading Node.JS Script:', response.statusText);
+        }
+    }
 
-	// Function to make the GM_xmlhttpRequest
-	function makeRequest() {
-			console.log('[test] make request');
-		GM_xmlhttpRequest({
-			method: 'GET',
-			url: nodeJSEndPoint,
-			onload: handleResponse,
-			onerror: handleResponse
-		});
-	}
+    // Function to make the GM_xmlhttpRequest
+    function makeRequest() {
+        console.log('[test] make request');
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: nodeJSEndPoint,
+            onload: handleResponse,
+            onerror: handleResponse
+        });
+    }
 
     makeRequest();
 }
