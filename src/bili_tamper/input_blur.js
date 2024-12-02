9d8ef1d8f1a06498a9df345c151acfc09a94606d
Yiang <yiangforwork@gmail.com>
2024-12-02 23:12:39 +0800

    update: hijack add_event_listener
    这里需要运行在document-start,在TamperMonkey里设置
    不过功能不太好实现,暂时搁置

--- src/bili_tamper/input_blur.js
+++ src/bili_tamper/input_blur.js
@@ -28,8 +28,23 @@ export function inputBlur() {
 		});
 	});
 
+	Element.prototype.old_add_event = Element.prototype.addEventListener;
+	Element.prototype.addEventListener = function(eventType, ...args) {
+		const el = this;
+
+		if( el.classList.contains("search-input-el") ) {
+			// 不允许search-input-el 添加 blur event
+			// 这里自定义了blur, 官方的会影响
+			if( eventType == "blur" ) {
+				// return;
+			}
+		}
+
+		Element.prototype.old_add_event.call( this, eventType, ...args );
+	}
+
 	listenForDOM(".search-input-el", (el) => {
-		el.addEventListener("blur", (e) => {
+		el.old_add_event("blur", (e) => {
 			if (
 				e.type === "blur" &&
 				e.relatedTarget &&
